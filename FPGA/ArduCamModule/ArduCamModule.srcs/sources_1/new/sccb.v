985efba0092eab49d0654b72bdc5f3c11604d833
Victoria Hall <vhall001@ucr.edu>
2017-11-16 14:25:43 -0800

    updated sccb

--- FPGA/ArduCamModule/ArduCamModule.srcs/sources_1/new/sccb.v
+++ FPGA/ArduCamModule/ArduCamModule.srcs/sources_1/new/sccb.v
@@ -23,97 +23,116 @@ module sccb #(parameter CLK = 24000000) (    // system clock at 24MHz
     input wire clk,
     input [15:0] addr_data;
     input wire sda_in,  // ack from slave
+    output wire reg_loc,    // location in cameraReg to get addr_data
     output wire scl,    // sccb clock
     output wire sda_out);   // sccb data time
 
     reg [7:0] addr; // address of camera reg
     reg [7:0] data; // data to modify camera reg
     
-    localparam CAMERA_ID = 0x42;    // Address to write to camera regs
+    localparam CAMERA_ID_W = 0x84;    // Address to write to camera regs
     localparam IDLE = 0;
     localparam START_TX = 1;
     localparam START_DATA_TX = 2;
-    localparam SEND_BIT = 3;
-    localparam STOP_DATA_TX = 4;
-    localparam STOP_TX = 5;
-    localparam WAIT_ACK = 6;
+    localparam LOAD_DATA = 3;
+    localparam SEND_BIT = 4;
+    localparam STOP_DATA_TX = 5;
+    localparam STOP_TX = 6;
+    localparam WAIT = 7;
 
     reg [2:0] state;    // FSM state
+    reg [2:0] state_return; // FSM state to return to after wait
     reg [1:0] phase;    // data tx phase
-    reg [3:0] i;    // data tx bit index 
+    reg [3:0] i;    // data tx bit index
+    reg [7:0] reg_loc_val;  // temp storage for reg_loc output
+    reg ready;
+    reg [5:0] wait_time;    // number clk periods to wait
 
     initial begin
         state = IDLE;   // init FSM state to idle
         scl = 1;
         sda_out = 1;
+        reg_loc_val = 0;    // start at first reg loc
         phase = 0;
         i = 7;  // data tx will be sent from MSB to LSB
+        ready = 1;
     end
 
     always@(posedge clk)
         case(state)
             IDLE: begin
-                if(sda_in == 1) begin // slave ack ready for data
-                    sda_out = 0;    // pull sda low to signal start tx
-                    state = START_TX;
+                reg_loc = reg_loc_val;  // set reg loc to update addr_data
+                if(ready) begin // ready to send new data
+                    state = START_TX;   // ready to start tx
+                    addr = addr_data[15:8]; // store current reg addr
+                    data = addr_data[7:0];  // store current reg data
+                    reg_loc_val++;  // increment to next reg location
+                    // need to wait 600ns before next signal (t_HD:STA)
+                    ready = 0;
                 end`
             end
 
             START_TX: begin
-                scl = 0;    // pull scl low to signal start data tx
-                addr = addr_data[15:8];
-                data = addr_data[7:0];
+                sda_out = 0;    // pull sda low to signal start of tx
+                wait_time = 22;  //  wait for sda_out to stabilize(max 7) and start condition hold time(min 15)
+                state = WAIT;
+                state_return = START_DATA_TX;
+              
+                wait_time = 7;// need to wait 300ns for scl to be stable low (t_F)
+                // need to wait 100-900ns for data out to be valid (t_AA)
+                // need to wait (t_LOW)1.3us - t_AA before scl pulls high
+                
                 state = START_DATA_TX;
             end
 
             START_DATA_TX: begin
-                scl = 1;    // pull high to send bit
-                state = SEND_BIT;
+                scl = 0;    // pull scl low to signal start of data tx
+                wait_time = 7;  // wait for scl to stabilize(max 7)
+                state = WAIT;
+                state_return = LOAD_DATA;
+                // scl stays high for 600ns (t_HIGH)
             end
 
-            SEND_BIT: begin
+            LOAD_DATA: begin
+                scl = 1;    // pull high to send bit
+                wait_time = 7;  // wait for scl to stabilize(max 7)
+                state = WAIT;
+                state_return = SEND_BIT;
                 case(phase)
                     0: begin
-                        sda_out = CAMERA_ID[i]; // send one bit
-                        if(i > 0) begin
-                            i--;    // decrement i to next bit
-                            state = START_DATA_TX;
-                        end
-                        else begin
-                            i = 7;
-                            phase++;
-                            state = WAIT_ACK;   // wait for 9th bit ack from slave
-                        end
-                        scl = 0;    // pull low for end of bit tx
+                        tx_data = CAMERA_ID_W;
                     end
-
+                    
                     1: begin
-                        sda_out = addr[i];
-                        if(i > 0) begin
-                            i--;
-                            state = START_DATA_TX;
-                        end
-                        else begin
-                            i = 7;
-                            phase++;
-                            state = WAIT_ACK;
-                        end
-                        scl = 0;
+                        tx_data = addr;
                     end
 
                     2: begin
-                        sda_out = data[i];
-                        if(i > 0) begin
-                            i--;
-                            state = START_DATA_TX;
-                        end
-                        else begin
-                            i = 7;
-                            phase++;
-                            state = WAIT_ACK;
-                        end
+                        tx_data = data;
                     end
                 endcase
+            end
+    
+            SEND_BIT: begin
+                if(i >= 0) begin
+                    sda_out = tx_data[i]
+                    i--;
+                end
+                else begin  // sent 8 bits
+                    sda_out = 0;    // don't care bit (end of phase)
+                    phase++;
+                end
+                if(phase < 3) begin // still in phase 
+                    wait_time = 7;  // wait for sda`
+                    state = WAIT;
+                    state_return = LOAD_DATA;
+            end
+
+            END_BIT: begin
+                scl = 0;
+                wait_time = 7;  // wait for scl to stabilize(max 7)
+                state = WAIT;
+                state_return = LOAD_DATA;
             end
 
             STOP_DATA_TX: begin
@@ -126,17 +145,12 @@ module sccb #(parameter CLK = 24000000) (    // system clock at 24MHz
                 sda_out = 1;    // pull high to stop tx
             end
 
-            WAIT_ACK: begin
-                if(sda_in) begin    // if recieved true ack signal
-                    if(phase > 2) begin // end of 3 phase tx
-                        state = STOP_DATA_TX;   // signal end of data tx
-                    end
-                    else begin  // more phases to tx
-                        state = START_DATA_TX;  // go back to start of transmission
-                    end
+            WAIT: begin
+                if(wait_time == 0) begin
+                    state = state_return;   // finished waiting; go to next step
                 end
                 else begin
-                    state = WAIT_ACK;   // wait until received ack from slave
+                    wait_time--;    // update wait time
                 end
             end
         endcase
