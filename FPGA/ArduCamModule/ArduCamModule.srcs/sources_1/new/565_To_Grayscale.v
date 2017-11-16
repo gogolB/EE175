f67e8e7ff32758c0df0663a168e50468cd45e34a
gogolB <sbhat006@ucr.edu>
2017-11-16 14:30:36 -0800

    Added RGB565 To Grayscale module

---
+++ FPGA/ArduCamModule/ArduCamModule.srcs/sources_1/new/565_To_Grayscale.v
@@ -0,0 +1,27 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 11/16/2017 02:28:27 PM
+// Design Name: 
+// Module Name: 565_To_Grayscale
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module RGB565_To_Grayscale(
+    input wire [15:0] PixelData,
+    output wire [7:0] Grayscale
+    );
+endmodule
