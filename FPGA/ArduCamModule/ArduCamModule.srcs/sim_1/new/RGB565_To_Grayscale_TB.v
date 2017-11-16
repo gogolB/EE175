f933ee9f728b483f0fc7fb193d086c0dd23be11f
gogolB <sbhat006@ucr.edu>
2017-11-16 15:51:05 -0800

    Finished TB for 565->Gray

--- FPGA/ArduCamModule/ArduCamModule.srcs/sim_1/new/RGB565_To_Grayscale_TB.v
+++ FPGA/ArduCamModule/ArduCamModule.srcs/sim_1/new/RGB565_To_Grayscale_TB.v
@@ -20,7 +20,36 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 
-module RGB565_To_Grayscale_TB(
+module RGB565_To_Grayscale_TB();
 
-    );
+    reg[15:0] inputPixelData;
+    wire[7:0] outputData;
+    
+    reg clock;
+    wire outputClock;
+    
+    // MUT: Module Under Test
+    RGB565_To_Grayscale MUT (.PixelData(inputPixelData), .Grayscale(outputData), .clockIn(clock), .clockOut(outputClock));
+    
+    
+    initial
+    begin
+        clock = 0;
+        
+        // Test 1
+        inputPixelData = 16'b0000000000000000;
+        #5;
+        
+        // Test 2
+        inputPixelData = 16'b0000100000100001;
+        #5;
+        
+        #100 $finish;
+    end
+    
+    // Tick the clock.
+    always
+        #1 clock = !clock;
+    
+
 endmodule
