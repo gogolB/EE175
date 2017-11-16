1f56d4d348108905521c3b9bcc2c9f1390b3ce23
gogolB <sbhat006@ucr.edu>
2017-11-16 15:06:21 -0800

    Added in RGB565->Grayscale module and its TB. Implementation of 565->Gray is done. Working on TB

---
+++ FPGA/ArduCamModule/ArduCamModule.srcs/sim_1/new/RGB565_To_Grayscale_TB.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 11/16/2017 03:04:38 PM
+// Design Name: 
+// Module Name: RGB565_To_Grayscale_TB
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module RGB565_To_Grayscale_TB(
+
+    );
+endmodule
